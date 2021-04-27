7f6f70acdb5adfafdc9df989c6e015e271f365a4
Isabella Paats <isabellapaats@hotmail.com>
2021-04-27 15:05:01 -0300

    tp 5

---
+++ TP5/ej6.py
@@ -0,0 +1,20 @@
+#6
+# Implementá una clase que represente una calculadora sencilla, que permita sumar, restar y multiplicar. Este objeto debe entender 
+# los siguientes mensajes:
+"""
+cargar(numero)
+sumar(numero)
+restar(numero)
+multiplicar(numero)
+valorActual()
+"""
+# Si se evalúa la siguiente secuencia: el resultado debe ser 24.
+
+calculadora = Calculadora
+calculadora.cargar(0)
+calculadora.sumar(4)
+calculadora.multiplicar(5)
+calculadora.restar(8)
+calculadora.multiplicar(2)
+calculadora.valorActual()
+
